c686669c6d286ddc6224b09c6f4fab184a1eee06
Cerridwen <ogriff17@gmail.com>
2020-03-06 17:28:11 -0500

    Trying to add sound to this

--- assets/New folder/javascript/game.js
+++ assets/New folder/javascript/game.js
@@ -1,11 +1,12 @@
 var wins = 0;
 var losses = 0;
-var computerNumber = 0;
+var computerNumber = 0; //our variables are here
 var totalScore = 0;
 var crystal1; 
 var crystal2;
 var crystal3;
 var crystal4;
+var magicGlitterSound;
 
 function addToScore (x) //this function receives the crystal number
 {
@@ -14,33 +15,42 @@ function addToScore (x) //this function receives the crystal number
     if (x == "3") {totalScore = totalScore + crystal3;}
     if (x == "4") {totalScore = totalScore + crystal4;}
   
+    //This is to calculate the wins or losses variables
+
     if (totalScore == computerNumber) {
-        wins++; 
+        wins++;
+        totalScore = 0; 
+        setRandom ();
     }
 
     if (totalScore > computerNumber) {
       losses++;
+      totalScore = 0;
+      MagicGlitterSund = loadSound ("../images/Magic Glitter.wav");
+      setRandom ();
     }
-      
-    
- 
 
     document.getElementById("Wins").innerHTML = "Wins: " + wins;
     document.getElementById("Losses").innerHTML = "Losses: " + losses;
+    document.getElementById("computerNumber").innerHTML = "" + computerNumber;
     document.getElementById("totalScore").innerHTML = "Total Score: " + totalScore;
 
 }
+function setRandom()
+{
+computerNumber = Math.floor(Math.random() * (120 - 19 + 1) + 19);
 
+crystal1 = Math.floor(Math.random() * (12 - 1 + 1) + 1);
+crystal2 = Math.floor(Math.random() * (12 - 1 + 1) + 1);
+crystal3 = Math.floor(Math.random() * (12 - 1 + 1) + 1);
+crystal4 = Math.floor(Math.random() * (12 - 1 + 1) + 1);
+}
 
-computerNumber = Math.floor(Math.random() * 120) + 19;
-crystal1 = Math.floor(Math.random() * 12) + 1;
-crystal2 = Math.floor(Math.random() * 12) + 1;
-crystal3 = Math.floor(Math.random() * 12) + 1;
-crystal4 = Math.floor(Math.random() * 12) + 1;
-
+setRandom (); //store the function and run it
 
 //totalScore = totalScore + crystal1;
 
+//These statements are only executed when the page loads the first time
 
 document.getElementById("Wins").innerHTML = "Wins: " + wins;
 document.getElementById("Losses").innerHTML = "Losses: " + losses;
